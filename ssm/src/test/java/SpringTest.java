9c284304a6f0c8656faad97661d5b05909322f68
JQuery134 <baohuajie134@163.com>
2018-12-03 18:02:50 +0800

    test
    test

--- ssm/src/test/java/SpringTest.java
+++ ssm/src/test/java/SpringTest.java
@@ -1,10 +1,13 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import ssmDemo.bean.Student;
 
 public class SpringTest {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Student student = (Student)context.getBean("student");
+        ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println(context1.equals(context2));
+        Student student = (Student)context1.getBean("student");
         System.out.println(student.toString());
     }
 }
